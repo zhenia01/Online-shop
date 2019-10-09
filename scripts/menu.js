8cf0fd292d451460896ac6e8f7e8bf87066ca469
Zhenia Borodaikevych <borodaykevich@gmail.com>
2019-10-09 23:45:23 +0300

    added pop-up menu on small screens & cart icon

---
+++ scripts/menu.js
@@ -0,0 +1,11 @@
+"use strict"
+
+$(function() {
+  $(".navbar-menu-button").on("click", () => {
+    $(".navbar-nav").toggleClass("menu-open");
+    $(".navbar-nav .nav-item").toggleClass("menu-open");
+  });
+})
+
+
+
