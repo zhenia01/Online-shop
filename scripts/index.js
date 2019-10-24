925a319b2f72cb2cc529812a87fecae5ae6dc135
Zhenia Borodaikevych <borodaykevich@gmail.com>
2019-10-24 20:04:28 +0300

    added possibility to change count of items in cart

--- scripts/index.js
+++ scripts/index.js
@@ -8,16 +8,16 @@ $(function () {
   });
   //////
 
-  // goods counts on the right of the cart
   let goodsCount = 0;
   let $goodsCountElement = $(".goods-count");
-  
+
   let $cart = $(".cart");
 
   $(".cart-img").on("click", () => {
     if (goodsCount > 0) {
       $("#cart-modal").modal();
       $cart.css("display", "grid");
+      $(".total-price").css("display", "initial");
     } else {
       $("#empty-cart-modal").modal();
     }
@@ -30,30 +30,93 @@ $(function () {
     $(".cart-img").css("right", ($goodsCountElement.width() + 10).toString() + "px");
 
     let $goods = $(event.currentTarget).parent().parent();
-    console.log($goods);
     let goodsName = $goods.children().eq(1).children(":first").children(":first").text();
+    let goodsPrice = $goods.children().eq(1).children().eq(1).children(":first").text();
+    goodsPrice = parseInt(goodsPrice);
+    console.log(goodsPrice);
 
     let isInCart = false;
+    let count = 1;
 
     for (let i of goodsInCart) {
-      if (i.name == goodsName) {
+      if (i.name === goodsName) {
         isInCart = true;
-        i.count++;
+        count = ++i.count;
         break;
       }
     }
+
     if (!isInCart) {
       goodsInCart.push({
         name: goodsName,
-        count: 1
+        count: 1,
+        price: goodsPrice
       })
-  
+
       $goods = $goods.clone();
       $goods.children().last().children().last().remove();
-      // $goods.append("<div class='count-setter'>KEK</div>")
+      $goods.append(`
+      <div class='cart-item-count-setter'>
+        <img src="./img/minus.png" alt="-" class="cart-item-dec-count">
+        <span class="cart-item-count">1</span>
+        <img src="./img/plus.png" alt="+" class="cart-item-inc-count">
+      </div>`);
       $cart.append($goods);
+    } else {
+      updateCountInCart(goodsName, count)
+    }
+
+    countTotalPrice(goodsInCart)
+  });
+
+  function countTotalPrice() {
+    let totalPrice = 0;
+
+    for (let i of goodsInCart) {
+      if ("price" in i) {
+        totalPrice += i.price * i.count;
+      }
+    }
+
+    $(".total-price").text(totalPrice.toString());
+  };
+
+  function updateCountInCart(goodsName, count) {
+    let $countElem = $cart.find(".card .card-body .card-title-link:contains(" + goodsName + ")").parent().parent().parent().children(".cart-item-count-setter");
+    $countElem.children().eq(1).text(count.toString());
+  }
+
+  // function deleteFromCart()
+
+  $cart.on("click", ".cart-item-inc-count", (event) => {
+    let goodsName = $(event.currentTarget).parent().siblings().eq(1).children(":first").children(":first").text();
+
+    for (let i = 0; i < goodsInCart.length; i++) {
+      if (goodsInCart[i].name === goodsName) {
+        if (++goodsInCart[i].count === 0) {
+          goodsInCart.splice(i, 1)
+        } else {
+          updateCountInCart(goodsName, goodsInCart[i].count);
+        }
+        break;
+      }
     }
   });
-})
+
+  $cart.on("click", ".cart-item-dec-count", (event) => {
+    let goodsName = $(event.currentTarget).parent().siblings().eq(1).children(":first").children(":first").text();
+
+    for (let i = 0; i < goodsInCart.length; i++) {
+      if (goodsInCart[i].name === goodsName) {
+        if (--goodsInCart[i].count === 0) {
+          goodsInCart.splice(i, 1)
+        } else {
+          updateCountInCart(goodsName, goodsInCart[i].count);
+        }
+        break;
+      }
+    }
+  });
+});
 
 
